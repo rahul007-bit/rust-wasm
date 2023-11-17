2a57130ec3add9239aa0a05cc84eee0380cf4106
rahul007-bit <yadavrahul8141@yahoo.com>
2023-11-17 19:28:01 +0530

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+use wasm_bindgen::prelude::*;
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec![SnakeCell(spawn_index)],
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub struct World {
+    width: usize,
+    snake: Snake,
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new() -> World {
+        World {
+            width: 8,
+            snake: Snake::new(10),
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+    }
+}
+
+// wasm-pack build --target web
